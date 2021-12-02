2864ff8f8586b944bd4536fbba0f2a62fdaffa16
Lee Ming Feng <S10219107@connect.np.edu.sg>
2021-12-03 00:18:38 +0800

    Add javascript for dropbox interactions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/** Manual SLideshow **/
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +27,44 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+/* Dropdown box */
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+var ad = document.getElementById('appDrop').addEventListener('click', changeContent, false);
+
+var sq = document.getElementById('sqlDrop').addEventListener('click', changeContent, true);
+
+
+function changeContent() {
+    if (document.querySelector('appRedesign') == 'none') {
+      document.querySelector('.sql').style.display = 'none';
+      document.querySelector('appRedesign').style.display = 'block';
+    }
+
+    else {
+      document.querySelector('appRedesign').style.display = 'none';
+      document.querySelector('.sql').style.display = 'block';
+    }
 }
